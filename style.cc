2024396b802e9113ebb12d227813d8b2016d5c16
Elvis Honorio <104358645+ElvisHonorio@users.noreply.github.com>
2024-10-07 11:00:42 -0300

    section

--- style.cc
+++ style.cc
@@ -1,4 +1,9 @@
 body{
-    color: white;
-    background-color: block;
+    color: rgb(255,255,255);
+    background-color: black;
+}
+section{
+    background:rgb(189,36,200);
+    peding-bottom: 80px;
+    peding-top: 80px
 }
